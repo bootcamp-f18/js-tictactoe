742863b7e1924bb16e70deaac3e47c276228f176
Janine Hempy <jhempy@gmail.com>
2018-10-03 14:28:51 -0400

    Adds logic for win conditions

--- src/js/main.js
+++ src/js/main.js
@@ -7,19 +7,59 @@ let turn = 0;
 document.onreadystatechange = function() {
     if (document.readyState == "interactive") {
         document.getElementById('restartButton').onclick = startGame;
-        for (let i = 0; i < spaces.length; i++) {
-            spaces[i].addEventListener('click', takeSpace);
-        }
-
+        startGame();
     }
 };
 
+
+
+
+function gameOver() {
+
+    // Spaces:
+    //  0  1  2
+    //  3  4  5
+    //  6  7  8
+
+
+    // Is this the last turn? If so, and no win, then game is over and its a draw
+
+
+    return (
+        checkLineForWin([0,1,2]) ||
+        checkLineForWin([3,4,5]) ||
+        checkLineForWin([6,7,8]) ||
+        checkLineForWin([0,3,6]) ||
+        checkLineForWin([1,4,7]) ||
+        checkLineForWin([2,5,8]) ||
+        checkLineForWin([0,4,8]) ||
+        checkLineForWin([2,4,6])
+    );
+
+}
+
+
+function checkLineForWin(line) {
+
+    let first = spaces[line[0]].innerHTML;
+    if (first != '' && first == spaces[line[1]].innerHTML && first == spaces[line[2]].innerHTML) {
+        return true;
+    }
+    return false;
+
+}
 
 function startGame() {
 
     // TODO: as we develop the game, we are probably going to
     // update this function to clear variables and reset state
 
+    turn = 0;
+    message.innerHTML = "It is X's turn!";
+    for (let i = 0; i < spaces.length; i++) {
+        spaces[i].addEventListener('click', takeSpace);
+        spaces[i].innerHTML = '';
+    }
 }
 
 function takeSpace() {
@@ -33,7 +73,16 @@ function takeSpace() {
     this.innerHTML = symbols[turn % 2];
     this.removeEventListener('click', takeSpace);
 
-    message.innerHTML = 'Now it\'s turn ' + turn;
+
+    if (gameOver()) {
+        console.log("GAME IS OVER!");
+    }
+    else {
+
+    }
+
+
+    message.innerHTML = "It is " + symbols[(turn + 1) % 2] + "'s turn!";
 
 
 }
