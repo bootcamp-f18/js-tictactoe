69010414d768afc761ab0bcae9dd7052118abe92
Janine Hempy <jhempy@gmail.com>
2018-10-03 12:04:51 -0400

    Makes board interactive

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
-// This is the main javascript file
+let spaces = document.getElementsByClassName('space');
+let message = document.getElementById('message');
+let symbols = [ 'O', 'X' ];
+let turn = 0;
+
+
+document.onreadystatechange = function() {
+    if (document.readyState == "interactive") {
+        document.getElementById('restartButton').onclick = startGame;
+        for (let i = 0; i < spaces.length; i++) {
+            spaces[i].addEventListener('click', takeSpace);
+        }
+
+    }
+};
+
+
+function startGame() {
+
+    // TODO: as we develop the game, we are probably going to
+    // update this function to clear variables and reset state
+
+}
+
+function takeSpace() {
+
+    turn++;
+
+    // These console statements highlight the difference between "this" and the event that calls the function
+    // console.log(this);
+    // console.log(event);
+
+    this.innerHTML = symbols[turn % 2];
+    this.removeEventListener('click', takeSpace);
+
+    message.innerHTML = 'Now it\'s turn ' + turn;
+
+
+}
